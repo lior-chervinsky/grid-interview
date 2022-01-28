9948d70cbbd8d8e88541674967bc72b6c607cdcf
Lior Chervinsky <lior.chervinsky@equalum.io>
2022-01-28 05:12:26 +0200

    - handle config to show grid header, grid data - support custom component for displaying column data

--- src/Grid.js
+++ src/Grid.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-const Grid = ({ config, data }) => (
-  <table>
-    <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    </tbody>
-  </table>
+const Grid = ({config, data}) => (
+    <table>
+        <thead>
+        <tr>
+            {config.map(({title}) => <th>{title}</th>)}
+        </tr>
+        </thead>
+        <tbody>
+        {
+            data.map((dataItem) =>
+                <tr>
+                    {
+                        config.map(({field, component}) =>
+                            <td>{component ? component({data: dataItem[field]}) : dataItem[field]}</td>
+                        )}
+                </tr>
+            )}
+        </tbody>
+    </table>
 );
 
 export default Grid;
